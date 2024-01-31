da6eb1423a6068f86c821c69c57ba7bd643109b4
kaz <kazuhiro_xenosaga@hotmail.co.jp>
2024-01-31 09:10:43 +0900

    msdbを変換するときにGuid.Parse("6DF5825B-945C-4081-A4E3-292556E99B99")の式木を作ると例外が起きた。これはReflection.Guid.ToArrayBytesno シグネチャにエラーがあったから。

--- LinqDB/Reflection/Guid.cs
+++ LinqDB/Reflection/Guid.cs
@@ -7,6 +7,6 @@ internal static class Guid{
     //public static readonly ConstructorInfo ctor_abcdefghijk= C(() => System.Guid.pa new System.Guid(0,0,0,0,0,0,0,0,0,0,0));
     //public static readonly ConstructorInfo ctor_input_format = C(() => System.Guid.ParseExact("",""));
     //public void gg(){System.Guid.NewGuid().ToByteArray
-    public static readonly MethodInfo ToByteArray = typeof(System.Guid).GetMethod("ToByteArray")!;
+    public static readonly MethodInfo ToByteArray =  M(() => new System.Guid(0,1,2,3,4,5,6,7,8,9,10).ToByteArray());//typeof(System.Guid).GetMethod("ToByteArray")!;
     public static readonly MethodInfo ToString_ = ToString<System.Guid>();
 }
